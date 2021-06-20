0687f2dbfe3ac83cbb4f0d01d7f066cb9b0abad7
DavidTG <3455@hotlbertonschool.com>
2021-06-19 18:06:25 -0700

    Task_7_Print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry pint
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int abc;
+
+	for (abc = 122 ; abc >= 97 ; abc--)
+	{
+		putchar(abc);
+	}
+	putchar('\n');
+	return (0);
+}

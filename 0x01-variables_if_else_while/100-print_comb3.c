e11e20fb975978160b0d4d7e6f6cd8fb7be093e0
DavidTG <3455@hotlbertonschool.com>
2021-06-21 21:56:35 -0700

    Task_10_print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Alwasy 0 (Succes)
+ */
+int main(void)
+{
+	int uni = '0';
+	int dec = '0';
+
+	while (uni <= '9')
+	{	
+		while (dec <='9');
+		{
+			if (!(uni > dec || uni == j))
+			{
+				putchar(uni);
+				putchar(dec);
+				if (uni == '8' && dec = '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			dec++
+		}
+		dec = 'o';
+		uni++;
+	}
+	return (0);
+}
+	

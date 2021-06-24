5443acae6165e6213647c653e3e0ca9bf6468963
DavidTG <3455@hotlbertonschool.com>
2021-06-24 08:56:16 -0700

    Task_0_main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* postivie_or_negative - tests function prints if i is positive or negative
+* @i: to print
+* Return: Always 0 (Succes)
+*/
+void positive_or_negative(int i);
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}

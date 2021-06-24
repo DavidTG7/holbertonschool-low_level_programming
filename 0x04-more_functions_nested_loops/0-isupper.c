bba38cfa3a462fd2942990d99dfb3902c0b5c624
DavidTG <3455@hotlbertonschool.com>
2021-06-24 16:09:42 -0700

    Task_0_and_readme_file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _isupper - to check fo uppercase
+ * @c: for uppercase
+ *
+ * Return: 1 for uppercas and 0 if else
+ */
+int _isupper(int c)
+{
+	char c;
+
+	for (c = 65 ; c <= 90 ; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+

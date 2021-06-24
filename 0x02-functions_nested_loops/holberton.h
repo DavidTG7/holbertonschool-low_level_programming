0e6b78ec4d82f50e9d4c5cb6df353fde611f315d
DavidTG <3455@hotlbertonschool.com>
2021-06-23 20:54:49 -0700

    Task_8_hours

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -28,4 +28,7 @@ int _abs(int);
 /* Including prototype to print the last digit of a number */
 int print_last_digit(int);
 
+/* Including prtotype that prints every minute of the day of Jack Bauer */
+void jack_bauer(void);
+
 #endif

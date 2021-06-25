758d7d8db3645ef20ee8cf504a802560c5cb7f63
DavidTG <3455@hotlbertonschool.com>
2021-06-25 13:54:19 -0700

    Task_4_print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - to print numbers without 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; if <= 9 ; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i);
+	}
+}

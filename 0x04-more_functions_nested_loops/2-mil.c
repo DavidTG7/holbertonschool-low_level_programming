d563bd5923ca4f2e7d5a213cccf6acdabd940417
DavidTG <3455@hotlbertonschool.com>
2021-06-24 19:47:56 -0700

    Task_2_mul

---
+++ 0x04-more_functions_nested_loops/2-mil.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - to multiply two integers
+ * @a: for intger a
+ * @b: for integer b
+ * Return: Always 0 succes
+ */
+int mul(int a, int b)
+{
+	mul = a * b;
+	return (0);
+}
+

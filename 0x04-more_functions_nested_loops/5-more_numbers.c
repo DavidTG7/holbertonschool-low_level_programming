cd0ba7dfa4db91f784ce9b777fcda9ce02e5f40a
DavidTG <3455@hotlbertonschool.com>
2021-06-25 14:06:54 -0700

    Task_5_more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * more_numbers - to print numbers from 0 to 14 for 10 times
+ */
+void more_numbers(void)
+{
+	int i;
+	int con;
+
+	while (con = 1)
+	{
+		for (i = '0' ; i <= '14' ; i++)
+		{
+			_putchar(i);
+		}
+		con++;
+	}
+	_putchar(10);
+}
